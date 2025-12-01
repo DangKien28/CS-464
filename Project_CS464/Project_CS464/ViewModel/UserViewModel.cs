17602bade0d4135ecc66e9129811c5806f30ffef
DangKien28 <trungkien228102005@gmail.com>
2025-12-02 01:54:06 +0700

    base MVVM

---
+++ Project_CS464/Project_CS464/ViewModel/UserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_CS464.ViewModel
+{
+    class UserViewModel
+    {
+        public static Model.UserDBEntities db = new Model.UserDBEntities();
+
+    }
+}
